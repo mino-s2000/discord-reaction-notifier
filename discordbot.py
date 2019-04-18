1096dab99ecd3d9fe976c00d657d42964c95f0fa
mino <mino.s2000@outlook.com>
2019-04-18 13:50:23 +0900

    add heroku env.

--- discordbot.py
+++ discordbot.py
@@ -3,8 +3,8 @@ from pytz import timezone
 from datetime import datetime
 
 # param
-cAnnounceId = 000000000000000000 # Channel ID to post the message you want to get a reaction from.
-cAnnounceBotId = 111111111111111111 # Channel ID posted by this BOT.
+getReactionChId = 'ENV_GET_REACTION_CH_ID' # Channel ID to post the message you want to get a reaction from.
+postNotifierChId = 'ENV_POST_NOTIFIER_CH_ID' # Channel ID posted by this BOT.
 
 # initialize client
 client = discord.Client()
@@ -15,9 +15,9 @@ async def on_ready():
 
 @client.event
 async def on_reaction_add(reaction, user):
-    if reaction.message.channel.id != cAnnounceId:
+    if reaction.message.channel.id != getReactionChId:
         return
-    cAnnounceBot = client.get_channel(cAnnounceBotId)
+    cAnnounceBot = client.get_channel(postNotifierChId)
     # Get the datetime when the message was posted.
     # 'reaction.message.created_at' is UTC. But not set timezone data.
     mDatetime = timezone('UTC').localize(reaction.message.created_at)
@@ -26,4 +26,4 @@ async def on_reaction_add(reaction, user):
     reply = f'{user} is reacted. Target Message Posted At: {mDatetime}'
     await cAnnounceBot.send(reply)
 
-client.run('token')
+client.run('ENV_DISCORD_BOT_TOKEN')
