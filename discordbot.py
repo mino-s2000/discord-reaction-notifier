9b933cb18c77d38e384af93edd0391699df573aa
Mino <mino.s2000@outlook.com>
2019-05-10 19:17:04 +0900

    bugfix

--- discordbot.py
+++ discordbot.py
@@ -3,10 +3,6 @@ import discord
 from pytz import timezone
 from datetime import datetime
 
-# param
-getReactionChId = int(os.environ.get('ENV_GET_REACTION_CH_ID')) # Channel ID to post the message you want to get a reaction from.
-postNotifierChId = int(os.environ.get('ENV_POST_NOTIFIER_CH_ID')) # Channel ID posted by this BOT.
-
 # initialize client
 client = discord.Client()
 
@@ -16,6 +12,10 @@ async def on_ready():
 
 @client.event
 async def on_reaction_add(reaction, user):
+    # param
+    getReactionChId = int(os.environ.get('ENV_GET_REACTION_CH_ID')) # Channel ID to post the message you want to get a reaction from.
+    postNotifierChId = int(os.environ.get('ENV_POST_NOTIFIER_CH_ID')) # Channel ID posted by this BOT.
+
     if reaction.message.channel.id != getReactionChId:
         return
     cAnnounceBot = client.get_channel(postNotifierChId)
