93d0e7ac9855420b6a2bf436357981399e6005d8
Mino <mino.s2000@outlook.com>
2020-01-31 19:42:44 +0900

    update add reaction action

--- discordbot.py
+++ discordbot.py
@@ -33,21 +33,50 @@ async def on_message(message):
     if message.channel.id != GET_REACTION_CHANNEL_ID:
         return
     members = message.guild.members
-    title = 'お知らせ'
-    description = message.content.replace('@', '')
+    title = 'お知らせを読んだ方／読んでない方'
+#    description = discord.utils.escape_mentions(message.content)
     embed = discord.Embed(
         title = title,
-        description = description,
+        description = f'お知らせ発信者: {message.author.name}',
         color = EMBED_COLOR,
         timestamp = datetime.utcnow()
     )
     embed.set_footer(text = f"Latest Edit: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}, User: Bot")
     embed.add_field(
-        name = 'クラメン一覧',
-        value = '\n'.join([m.name for m in [m for m in members if m.name != message.author.name] for r in m.roles if r.id == MEMBER_ROLE_ID])
+        name = '読んでない方',
+        value = '\n'.join([m.name for m in [m for m in members if m.name != message.author.name] for r in m.roles if r.id == MEMBER_ROLE_ID]),
+        inline = True
     )
-    bot_message = await client.get_channel(POST_NOTIFIER_CHANNEL_ID).send(embed = embed)
-    r.set(message.id, bot_message.id)
+    embed.add_field(
+        name = '読んだ方',
+        value = '',
+        inline = True
+    )
+    embed_message = await message.channel.send(embed = embed)
+    r.set(embed_message.id, bot_message.id)
+
+@client.event
+async def on_raw_reaction_add(payload):
+    if payload.channel_id != GET_REACTION_CHANNEL_ID:
+        return
+    channel = client.get_channel(payload.channel_id)
+    target_msg = channel.fetch_message(int(r.get(payload.message_id)))
+    msg_embed = target_msg.embeds[0]
+    user = client.get_user(payload.user_id)
+    unread_field = msg_embed.fields[0]
+    read_field = msg_embed.fields[1]
+
+    unread_users = unread_field.value.split('\n')
+    if (user.name not in unread_users):
+        return
+    unread_users = '\n'.join([u for u in unread_users if user.name != u])
+
+    read_users = '\n'.join([read_field.value.split('\n'), user.name])
+
+    msg_embed.set_field_at(index = 0, name = unread_field.name, value = unread_users, inline = True)
+    msg_embed.set_field_at(index = 1, name = read_field.name, value = read_users, inline = True)
+    msg_embed.set_footer(text = f"Latest Edit: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}, User: {user.name}")
+    await target_msg.edit(embed = msg_embed)
 
 @client.event
 async def on_reaction_add(reaction, user):
